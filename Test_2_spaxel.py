b025c03dbf92da3af786054158ea457a0b4765c1
galgodon <43650741+galgodon@users.noreply.github.com>
2019-01-30 01:33:14 -0800

    Updated table code
    Read in all galaxy data at once rather than just spaxel by spaxel (but I still have to input data into table row by row because of how astropy.table works). Removed BIN coord and added z_vel, gal_red_B-V, ha_vel, and ha_vel_mask to columns.

--- Test_2_spaxel.py
+++ Test_2_spaxel.py
@@ -15,10 +15,10 @@ warnings.filterwarnings("ignore")
 
 plate = np.array([7977,8143])
 ifu = np.array([12705,3701])
-dtype = [('Plate', 'i4'), ('IFU', 'i4'), ('stell_vel_ID', 'f8'),('emline_ID','f8'),('rad_norm_spx','f8'),
-         ('azimuth_spx','f8'),('rad_norm_bin','f8'),('azimuth_bin','f8'),('snr_spx','f8'),
-         ('snr_bin','f8'),('stell_vel','f8'),('stell_vel_mask','f8'),('stell_sigma_cor','f8'),
-         ('stell_sigma_mask','f8'),('spec_index','f8'),('spec_index_err','f8'),('spec_index_mask','f8')]
+dtype = [('plate', 'i4'), ('ifu', 'i4'),('z_vel', 'f8'),('gal_red_B-V', 'f8'), ('stell_vel_ID', 'f8'),('emline_ID','f8'),('rad_norm_spx','f8'),
+         ('azimuth_spx','f8'),('snr_spx','f8'),
+         ('snr_bin','f8'),('stell_vel','f8'),('stell_vel_mask','f8'),('ha_vel', 'f8'),('ha_vel_mask', 'f8'),('stell_sigma_cor','f8'),
+         ('stell_sigma_mask','f8'),('spec_index_D4000','f8'),('spec_index_ivar_D4000','f8'),('spec_index_mask_D4000','f8'),('spec_index_HDeltaA', 'f8'),('spec_index_ivar_HDeltaA', 'f8'),('spec_index_mask_HDeltaA', 'f8')]
 emline_names = np.array(['H_alpha','H_beta','OII-3727','OII-3729','NII-6549','SII-6718','SII-6732','OIII-4960','OIII-5008','OI-6302','OI-6365'])
 emline_indx = np.array([18,11,0,1,17,20,21,12,13,15,16])  # corresponding index numbers found on https://trac.sdss.org/wiki/MANGA/TRM/TRM_MPL-7/DAPDataModel#DAPMAPSfile
 assert len(emline_names)==len(emline_indx)
@@ -31,60 +31,71 @@ for k in range(len(emline_names)):
 table = Table(dtype=dtype)
 
 for i in range(len(plate)):
+    
     print('Galaxy {}/{}'.format(i+1,len(plate)))
+    
     maps_file = 'data/manga-{0}-{1}-MAPS-HYB10-GAU-MILESHC.fits.gz'.format(plate[i],ifu[i])
     maps = fits.open(maps_file)
     
-    tot_spaxel = len(maps['BINID'].data[1,:,:].flatten())   # total number of spaxels in this galaxy
-# =============================================================================
-#     row.append(.flatten()[l])  
-# =============================================================================
-    for l in range(tot_spaxel):    # loop through each sapxel
-        sys.stdout.write('\r'+('   Importing data from spaxel {}/{}'.format(l+1,tot_spaxel)))
-        row = []
-        row.append(plate[i])
-        row.append(ifu[i])
+    good_spaxel = (maps['BINID'].data[0,:,:].flatten() != -1)   # 
+
+    row = np.zeros((good_spaxel.sum(),len(dtype)))
     
-        # Get Bin IDs we want
-        row.append(maps['BINID'].data[1,:,:].flatten()[l])
-        row.append(maps['BINID'].data[3,:,:].flatten()[l])
+    row[:,0] = (np.full(good_spaxel.sum(),plate[i]))       
+    row[:,1] = (np.full(good_spaxel.sum(),ifu[i]))
     
-        # Get coordinates
-        row.append(maps['SPX_ELLCOO'].data[1,:,:].flatten()[l])  # radius normalized by the elliptical Petrosian effective radius from the NSA
-        row.append(maps['SPX_ELLCOO'].data[2,:,:].flatten()[l])  # azimuth angle
-        
-        row.append(maps['BIN_LWELLCOO'].data[1,:,:].flatten()[l])
-        row.append(maps['BIN_LWELLCOO'].data[2,:,:].flatten()[l])
-        
-        # Get continuum signal to noise ratio
-        row.append(maps['SPX_SNR'].data.flatten()[l])  # S/N in each spaxel (emission-line measurements done per spaxel)
-        row.append(maps['BIN_SNR'].data.flatten()[l])  # S/N in each bin (stellar measurements done per bin)
-        
-        # Get stellar velocity
-        row.append(maps['STELLAR_VEL'].data.flatten()[l])
-        row.append(maps['STELLAR_VEL_MASK'].data.flatten()[l])
-        
-        # Get corrected velocity dispersion
-        row.append((np.sqrt( (maps['STELLAR_SIGMA'].data)**2 - (maps['STELLAR_SIGMACORR'].data)**2 )).flatten()[l])
-        row.append(maps['STELLAR_SIGMA_MASK'].data.flatten()[l])
-        
-        # Get spectral index for D4000 (indx 43, channel 44)
-        row.append(maps['SPECINDEX'].data[43,:,:].flatten()[l])
-        row.append(np.power(maps['SPECINDEX_IVAR'].data[43,:,:],-1/2).flatten()[l])
-        row.append(maps['SPECINDEX_MASK'].data[43,:,:].flatten()[l])
+    row[:,2] = (np.full(good_spaxel.sum(),maps[0].header['SCINPVEL']))
+    row[:,3] = (np.full(good_spaxel.sum(),maps[0].header['EBVGAL']))
+
+    # Get Bin IDs we want
+    row[:,4] = (maps['BINID'].data[1,:,:].flatten()[good_spaxel])
+    row[:,5] = (maps['BINID'].data[3,:,:].flatten()[good_spaxel])
+
+    # Get coordinates
+    row[:,6] = (maps['SPX_ELLCOO'].data[1,:,:].flatten()[good_spaxel])  # radius normalized by the elliptical Petrosian effective radius from the NSA
+    row[:,7] = (maps['SPX_ELLCOO'].data[2,:,:].flatten()[good_spaxel])  # azimuth angle
     
-        # Get emmision line properties of Ha, hb, o2, n2, s2, o3, o1
+    # Get continuum signal to noise ratio
+    row[:,8] = (maps['SPX_SNR'].data.flatten()[good_spaxel])  # S/N in each spaxel (emission-line measurements done per spaxel)
+    row[:,9] = (maps['BIN_SNR'].data.flatten()[good_spaxel])  # S/N in each bin (stellar measurements done per bin)
+    
+    # Get stellar velocity
+    row[:,10] = (maps['STELLAR_VEL'].data.flatten()[good_spaxel])
+    row[:,11] = (maps['STELLAR_VEL_MASK'].data.flatten()[good_spaxel])
+    
+    # Get H_alpha velocity
+    row[:,12] = (maps['EMLINE_GVEL'].data[18,:,:].flatten()[good_spaxel])
+    row[:,13] = (maps['EMLINE_GVEL_MASK'].data[18,:,:].flatten()[good_spaxel])
+
+    # Get corrected velocity dispersion
+    row[:,14] = ((np.sqrt( (maps['STELLAR_SIGMA'].data)**2 - (maps['STELLAR_SIGMACORR'].data)**2 )).flatten()[good_spaxel])
+    row[:,15] = (maps['STELLAR_SIGMA_MASK'].data.flatten()[good_spaxel])
+    
+    # Get spectral index for D4000 (indx 43, channel 44)
+    row[:,16] = (maps['SPECINDEX'].data[43,:,:].flatten()[good_spaxel])
+    row[:,17] = (maps['SPECINDEX_IVAR'].data[43,:,:].flatten()[good_spaxel])
+    row[:,18] = (maps['SPECINDEX_MASK'].data[43,:,:].flatten()[good_spaxel])
+
+    # Get spectral index for HDeltaA (indx 21, channel 22)
+    row[:,19] = (maps['SPECINDEX'].data[21,:,:].flatten()*maps['SPECINDEX_CORR'].data[21,:,:].flatten())[good_spaxel]
+    row[:,20] = (maps['SPECINDEX_IVAR'].data[21,:,:].flatten()[good_spaxel])
+    row[:,21] = (maps['SPECINDEX_MASK'].data[21,:,:].flatten()[good_spaxel])
+    
+    # Get emmision line properties of Ha, hb, o2, n2, s2, o3, o1
+    
+    for j in range(len(emline_names)):
+        row[:,22+5*j] = (maps['EMLINE_SEW'].data[emline_indx[j],:,:].flatten()[good_spaxel])          # summed equivalent width
+        row[:,23+5*j] = (maps['EMLINE_SEW_MASK'].data[emline_indx[j],:,:].flatten()[good_spaxel])            
+        row[:,24+5*j] = (maps['EMLINE_GFLUX'].data[emline_indx[j],:,:].flatten()[good_spaxel])        # gaussian flux
+        row[:,25+5*j] = (maps['EMLINE_GFLUX_IVAR'].data[emline_indx[j],:,:].flatten()[good_spaxel])   # gaussian flux error
+        row[:,26+5*j] = (maps['EMLINE_GFLUX_MASK'].data[emline_indx[j],:,:].flatten()[good_spaxel])   # same mask for flux and error
         
-        for j in range(len(emline_names)):
-            row.append(maps['EMLINE_SEW'].data[emline_indx[j],:,:].flatten()[l])                        # summed equivalent width
-            row.append(maps['EMLINE_SEW_MASK'].data[emline_indx[j],:,:].flatten()[l])            
-            row.append(maps['EMLINE_GFLUX'].data[emline_indx[j],:,:].flatten()[l])                      # gaussian flux
-            row.append(np.power(maps['EMLINE_GFLUX_IVAR'].data[emline_indx[j],:,:],-1/2).flatten()[l])  # gaussian flux error
-            row.append(maps['EMLINE_GFLUX_MASK'].data[emline_indx[j],:,:].flatten()[l])                 # same mask for flux and error
-        
-        table.add_row(row)
-#table.write('test.fits', format='fits')
-#ascii.write(table,'test.dat')
+    for l in range(len(row)):
+        sys.stdout.write('\r'+('  adding row {}/{}'.format(l+1,len(row))))
+        table.add_row(row[l])
+    print()
+
+table.write('test2.fits', format='fits')
     
     
     
--- table_2_by_spaxel.py
+++ table_2_by_spaxel.py
@@ -36,10 +36,10 @@ ifu = log[1].data['IFUDESIGN'][indx]
 # Initialize Empty Table
 # =============================================================================
 
-dtype = [('Plate', 'i4'), ('IFU', 'i4'), ('stell_vel_ID', 'f8'),('emline_ID','f8'),('rad_norm_spx','f8'),
-         ('azimuth_spx','f8'),('rad_norm_bin','f8'),('azimuth_bin','f8'),('snr_spx','f8'),
-         ('snr_bin','f8'),('stell_vel','f8'),('stell_vel_mask','f8'),('stell_sigma_cor','f8'),
-         ('stell_sigma_mask','f8'),('spec_index','f8'),('spec_index_err','f8'),('spec_index_mask','f8')]
+dtype = [('plate', 'i4'), ('ifu', 'i4'),('z_vel', 'f8'),('gal_red_B-V', 'f8'), ('stell_vel_ID', 'f8'),('emline_ID','f8'),('rad_norm_spx','f8'),
+         ('azimuth_spx','f8'),('snr_spx','f8'),('snr_bin','f8'),('stell_vel','f8'),('stell_vel_mask','f8'),('ha_vel', 'f8'),('ha_vel_mask', 'f8'),
+         ('stell_sigma_cor','f8'),('stell_sigma_mask','f8'),('spec_index_D4000','f8'),('spec_index_ivar_D4000','f8'),('spec_index_mask_D4000','f8'),
+         ('spec_index_HDeltaA', 'f8'),('spec_index_ivar_HDeltaA', 'f8'),('spec_index_mask_HDeltaA', 'f8')]
 emline_names = np.array(['H_alpha','H_beta','OII-3727','OII-3729','NII-6549','SII-6718','SII-6732','OIII-4960','OIII-5008','OI-6302','OI-6365'])
 emline_indx = np.array([18,11,0,1,17,20,21,12,13,15,16])  # corresponding index numbers found on https://trac.sdss.org/wiki/MANGA/TRM/TRM_MPL-7/DAPDataModel#DAPMAPSfile
 assert len(emline_names)==len(emline_indx)
@@ -57,59 +57,74 @@ table = Table(dtype=dtype)
 
 for i in range(len(plate)):
     
-    print('Galaxy {}/{}'.format(i+1,len(plate)))
-    maps_file = '/data/manga/spectro/analysis/MPL-7/HYB10-GAU-MILESHC/{0}/{1}/manga-{0}-{1}-MAPS-HYB10-GAU-MILESHC.fits.gz'.format(plate[i],ifu[i])
+    print('Galaxy {}/{}'.format(i+1,len(plate)))  # print statement to visually check progress
+    
+    maps_file = 'data/manga-{0}-{1}-MAPS-HYB10-GAU-MILESHC.fits.gz'.format(plate[i],ifu[i])  # load in maps file for this galaxy
     maps = fits.open(maps_file)
     
-    tot_spaxel = len(maps['BINID'].data[1,:,:].flatten())   # total number of spaxels in this galaxy
-    
-    for l in range(tot_spaxel):    # loop through each spaxel
+    good_spaxel = (maps['BINID'].data[0,:,:].flatten() != -1)   # ignore useless spaxels (like the corners)
 
-        sys.stdout.write('\r'+('   Importing data from spaxel {}/{}'.format(l+1,tot_spaxel)))
-        row = []                   # initialize row, we will append the data for each column
-        row.append(plate[i])       # note plate and ifu are indexed by 'i', this makes it so every spaxel in the 
-        row.append(ifu[i])            # same galaxy has the same plate and ifu
+    row = np.zeros((good_spaxel.sum(),len(dtype)))  # initialize array where we will be storing row data
     
-        # Get Bin IDs we want
-        row.append(maps['BINID'].data[1,:,:].flatten()[l])   # stellar velocity ID
-        row.append(maps['BINID'].data[3,:,:].flatten()[l])   # emline and d4000 ID
+    # Get Plate and IFU
+    row[:,0] = (np.full(good_spaxel.sum(),plate[i]))  # plate and ifu is the same for every spaxel which is why I use np.full     
+    row[:,1] = (np.full(good_spaxel.sum(),ifu[i]))
     
-        # Get coordinates
-        row.append(maps['SPX_ELLCOO'].data[1,:,:].flatten()[l])  # radius normalized by the elliptical Petrosian effective radius from the NSA
-        row.append(maps['SPX_ELLCOO'].data[2,:,:].flatten()[l])  # azimuth angle
-        
-        row.append(maps['BIN_LWELLCOO'].data[1,:,:].flatten()[l])
-        row.append(maps['BIN_LWELLCOO'].data[2,:,:].flatten()[l])
-        
-        # Get continuum signal to noise ratio
-        row.append(maps['SPX_SNR'].data.flatten()[l])  # S/N in each spaxel (emission-line measurements done per spaxel)
-        row.append(maps['BIN_SNR'].data.flatten()[l])  # S/N in each bin (stellar measurements done per bin)
-        
-        # Get stellar velocity
-        row.append(maps['STELLAR_VEL'].data.flatten()[l])
-        row.append(maps['STELLAR_VEL_MASK'].data.flatten()[l])
-        
-        # Get corrected velocity dispersion
-        row.append((np.sqrt( (maps['STELLAR_SIGMA'].data)**2 - (maps['STELLAR_SIGMACORR'].data)**2 )).flatten()[l])
-        row.append(maps['STELLAR_SIGMA_MASK'].data.flatten()[l])
-        
-        # Get spectral index for D4000 (indx 43, channel 44)
-        row.append(maps['SPECINDEX'].data[43,:,:].flatten()[l])
-        row.append(np.power(maps['SPECINDEX_IVAR'].data[43,:,:],-1/2).flatten()[l])
-        row.append(maps['SPECINDEX_MASK'].data[43,:,:].flatten()[l])
+    # Get z_vel and gal_red_B-V
+    row[:,2] = (np.full(good_spaxel.sum(),maps[0].header['SCINPVEL']))
+    row[:,3] = (np.full(good_spaxel.sum(),maps[0].header['EBVGAL']))
+
+    # Get Bin IDs we want
+    row[:,4] = (maps['BINID'].data[1,:,:].flatten()[good_spaxel])  # stellar
+    row[:,5] = (maps['BINID'].data[3,:,:].flatten()[good_spaxel])  # emline
+
+    # Get coordinates
+    row[:,6] = (maps['SPX_ELLCOO'].data[1,:,:].flatten()[good_spaxel])  # radius normalized by the elliptical Petrosian effective radius from the NSA
+    row[:,7] = (maps['SPX_ELLCOO'].data[2,:,:].flatten()[good_spaxel])  # azimuth angle
     
-        # Get emmision line properties of Ha, hb, o2, n2, s2, o3, o1
-        
-        for j in range(len(emline_names)):
-            row.append(maps['EMLINE_SEW'].data[emline_indx[j],:,:].flatten()[l])                        # summed equivalent width
-            row.append(maps['EMLINE_SEW_MASK'].data[emline_indx[j],:,:].flatten()[l])            
-            row.append(maps['EMLINE_GFLUX'].data[emline_indx[j],:,:].flatten()[l])                      # gaussian flux
-            row.append(np.power(maps['EMLINE_GFLUX_IVAR'].data[emline_indx[j],:,:],-1/2).flatten()[l])  # gaussian flux error
-            row.append(maps['EMLINE_GFLUX_MASK'].data[emline_indx[j],:,:].flatten()[l])                 # same mask for flux and error
-        
-        table.add_row(row)
+    # Get continuum signal to noise ratio
+    row[:,8] = (maps['SPX_SNR'].data.flatten()[good_spaxel])  # S/N in each spaxel (emission-line measurements done per spaxel)
+    row[:,9] = (maps['BIN_SNR'].data.flatten()[good_spaxel])  # S/N in each bin (stellar measurements done per bin)
     
-table.write('data/spaxel_data_table.fits', format='fits')
+    # Get stellar velocity
+    row[:,10] = (maps['STELLAR_VEL'].data.flatten()[good_spaxel])
+    row[:,11] = (maps['STELLAR_VEL_MASK'].data.flatten()[good_spaxel])
+    
+    # Get H_alpha velocity (indx 18, channel 19)
+    row[:,12] = (maps['EMLINE_GVEL'].data[18,:,:].flatten()[good_spaxel]) 
+    row[:,13] = (maps['EMLINE_GVEL_MASK'].data[18,:,:].flatten()[good_spaxel])
+
+    # Get corrected velocity dispersion
+    row[:,14] = ((np.sqrt( (maps['STELLAR_SIGMA'].data)**2 - (maps['STELLAR_SIGMACORR'].data)**2 )).flatten()[good_spaxel])
+    row[:,15] = (maps['STELLAR_SIGMA_MASK'].data.flatten()[good_spaxel])
+    
+    # Get spectral index for D4000 (indx 43, channel 44)
+    row[:,16] = (maps['SPECINDEX'].data[43,:,:].flatten()[good_spaxel])
+    row[:,17] = (maps['SPECINDEX_IVAR'].data[43,:,:].flatten()[good_spaxel])
+    row[:,18] = (maps['SPECINDEX_MASK'].data[43,:,:].flatten()[good_spaxel])
+
+    # Get spectral index for HDeltaA (indx 21, channel 22) note: this has a correction but D4000 didn't
+    row[:,19] = (maps['SPECINDEX'].data[21,:,:].flatten()*maps['SPECINDEX_CORR'].data[21,:,:].flatten())[good_spaxel]
+    row[:,20] = (maps['SPECINDEX_IVAR'].data[21,:,:].flatten()[good_spaxel])
+    row[:,21] = (maps['SPECINDEX_MASK'].data[21,:,:].flatten()[good_spaxel])
+    
+    # Get emmision line properties of Ha, hb, o2, n2, s2, o3, o1
+    
+    for j in range(len(emline_names)):
+        row[:,22+5*j] = (maps['EMLINE_SEW'].data[emline_indx[j],:,:].flatten()[good_spaxel])          # summed equivalent width
+        row[:,23+5*j] = (maps['EMLINE_SEW_MASK'].data[emline_indx[j],:,:].flatten()[good_spaxel])            
+        row[:,24+5*j] = (maps['EMLINE_GFLUX'].data[emline_indx[j],:,:].flatten()[good_spaxel])        # gaussian flux
+        row[:,25+5*j] = (maps['EMLINE_GFLUX_IVAR'].data[emline_indx[j],:,:].flatten()[good_spaxel])   # gaussian flux error
+        row[:,26+5*j] = (maps['EMLINE_GFLUX_MASK'].data[emline_indx[j],:,:].flatten()[good_spaxel])   # same mask for flux and error
+        
+    for l in range(len(row)):  # still have to add to table row by row
+        sys.stdout.write('\r'+('  adding row {}/{}'.format(l+1,len(row))))   # print statement to visually check progress
+        table.add_row(row[l])
+    print()  # goes to next line to look more visually pleasing
+    
+table.write('data/spaxel_data_table.fits', format='fits',overwrite=True)
+
+# mask>0 and ivar >0
     
     
     
